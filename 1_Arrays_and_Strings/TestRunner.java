d61c7925efb4b561dfd4f34722859395afe7743f
Adam Freymiller <ADAM.FREYMILLER@morningstar.com>
2018-12-24 17:50:13 -0600

    adding initial test

--- 1_Arrays_and_Strings/TestRunner.java
+++ 1_Arrays_and_Strings/TestRunner.java
@@ -1,10 +1,14 @@
 public class TestRunner {
   public static void main(String[] args) {
       
-    ArraysAndStrings isP = new ArraysAndStrings();
+    ArraysAndStrings problems = new ArraysAndStrings();
 
     // Prints "Hello, World" to the terminal window.
     System.out.println("Hello, World");
+
+    boolean outcome = problems.isPermutation("hllo", "world");
+
+    System.out.println(outcome);
 
     // isPermutation("hello", "world");
   }
