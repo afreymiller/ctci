e48e88dbd24484be3645fe25857c32ea059243d6
Adam Freymiller <ADAM.FREYMILLER@morningstar.com>
2018-12-24 17:42:26 -0600

    organizing the directories

---
+++ 1_Arrays_and_Strings/IsPermutation.java
@@ -0,0 +1,10 @@
+public class IsPermutation {
+
+    public boolean isPermutation (String a, String b) {
+        if (a.length() != b.length()) {
+            return false;
+        }
+
+        return true;
+    }
+}
---
+++ 1_Arrays_and_Strings/TestRunner.java
@@ -0,0 +1,11 @@
+public class TestRunner {
+  public static void main(String[] args) {
+      
+    IsPermutation isP = new IsPermutation();
+
+    // Prints "Hello, World" to the terminal window.
+    System.out.println("Hello, World");
+
+    // isPermutation("hello", "world");
+  }
+}
