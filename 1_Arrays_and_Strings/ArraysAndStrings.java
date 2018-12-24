f818dfb7d916b72e574ee98260210d853c09df16
Adam Freymiller <ADAM.FREYMILLER@morningstar.com>
2018-12-24 17:46:53 -0600

    renaming more things

---
+++ 1_Arrays_and_Strings/ArraysAndStrings.java
@@ -0,0 +1,15 @@
+public class ArraysAndStrings {
+
+    ArraysAndStrings() {
+        /* TODO: Find a more meaningful constructor */
+        System.out.println("Arrays and Strings object instantiated");
+    }
+
+    public boolean isPermutation (String a, String b) {
+        if (a.length() != b.length()) {
+            return false;
+        }
+
+        return true;
+    }
+}
--- 1_Arrays_and_Strings/TestRunner.java
+++ 1_Arrays_and_Strings/TestRunner.java
@@ -1,7 +1,7 @@
 public class TestRunner {
   public static void main(String[] args) {
       
-    IsPermutation isP = new IsPermutation();
+    ArraysAndStrings isP = new ArraysAndStrings();
 
     // Prints "Hello, World" to the terminal window.
     System.out.println("Hello, World");
