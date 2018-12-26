f8b8ed7a8bde645049bb29aef64508aa30dcf8ae
Adam Freymiller <ADAM.FREYMILLER@morningstar.com>
2018-12-25 21:20:34 -0600

    implemented remove duplicates

--- 2_Linked_Lists/LinkedListTest.java
+++ 2_Linked_Lists/LinkedListTest.java
@@ -1,20 +1,15 @@
-import java.util.HashMap;
 
 public class LinkedListTest {
   public static void main(String [] args) {
     LinkedList ll = new LinkedList();
-    // ll.traverseList();
 
     ll.appendToTail(4);
     ll.appendToTail(5);
+    ll.appendToTail(3);
+    ll.appendToTail(4);
 
-    HashMap<String, String> capitalCities = new HashMap<String, String>();
+    ll.removeDuplicates();
 
-    // Add keys and values (Country, City)
-    capitalCities.put("England", "London");
-    capitalCities.put("Germany", "Berlin");
-    capitalCities.put("Norway", "Oslo");
-    capitalCities.put("USA", "Washington DC");
-    System.out.println(capitalCities); 
+    ll.traverseList();
   }
 }
